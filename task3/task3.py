7b1e099bf4c245560882c9cdf5adb1f6eab21709
Mamias1337 <mamias.nuif@gmail.com>
2025-05-18 22:33:50 +0400

    fix

--- task3/task3.py
+++ task3/task3.py
@@ -1,8 +1,20 @@
 import json
 import sys
+import os
 
+
+    # Проверка на количество аргументов.
 if len(sys.argv) !=4:
     print('Использование: python task3.py <tests.json> <values.json> <report.json>')
+    sys.exit(1)
+
+    # Проверка наличия файлов.
+if not os.path.isfile(sys.argv[1]):
+    print('Файл tests.json не существует.')
+    sys.exit(1)
+
+if not os.path.isfile(sys.argv[2]):
+    print('Файл values.json не существует.')
     sys.exit(1)
 
     # Загрузка данных из файлов
@@ -14,6 +26,7 @@ with open(sys.argv[2], "r") as f:  # values.json
 
     # Преобразуем values в словарь для быстрого доступа
 values_list = {item["id"]: item["value"] for item in values_data["values"]}
+
 
     # Рекурсивная функция для вставки значений
 def fill_values(tests):
@@ -30,3 +43,5 @@ fill_values(tests_data["tests"])
     # Сохранение результата в report.json
 with open(sys.argv[4], "w") as f:
     json.dump(tests_data, f, indent=4)
+
+    print('report.json успешно изменён.')
